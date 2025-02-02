5a3294439f38cb1d3af8b525c162084f8b38ef6a
Darren Croton <2062219+darrencroton@users.noreply.github.com>
2025-02-02 13:45:31 +1100

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -11,8 +11,8 @@ window.createWeekSummary = function(week, weekNumber, totalWeeks, useKm) {
     summaryEl.innerHTML = `
         <div class="week-summary-title">Week ${weekNumber} of ${totalWeeks}</div>
         <div class="week-dates">${getDateRange(week)}</div>
-        <div class="week-countdown">${weeksUntilRace} week(s) until race day</div>
         <div class="total-distance">Weekly distance: ${distance}km</div>
+        <div class="week-countdown">${weeksUntilRace} week(s) until race day</div>
     `;
 
     return summaryEl;
